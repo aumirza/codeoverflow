57bcc8d363c58b630fc962faf109cf595741c32d
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-04-21 21:42:09 +0530

    feat: update dependencies and configuration for Tailwind CSS and PostCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -101,7 +100,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
